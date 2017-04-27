8e3b22894123113ae2f29e3df1bab9cbfd1d54db
zhengyan.gao <zhengyan.gao@zhengyangaodeMacBook-Pro.local>
2017-04-27 11:20:46 +0800

    init project

---
+++ btcbot/lib/api/btccomapi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class btccomapi():
+    def __init__(self):
+        pass
+
+    def _request(self):
+        pass
+
+    def get_block(self):
+        pass
+
+    def get_tx(self):
+        pass
+
+    def get_address(self):
+        pass
